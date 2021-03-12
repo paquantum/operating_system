2ae43972618c85327d0c3b915c605c38492a5f36
Park Minwoo <enmw34@naver.com>
2021-03-12 18:09:49 +0900

    thread synchronization
    - 동기화

---
+++ OS/pthread_synchronization2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int sum;
+
+void* run1(void* param) {
+    int i;
+    for (i = 0; i < 10000; i++)
+        sum += 1;
+    pthread_exit(0);
+}
+
+void* run2(void* param) {
+    int i;
+    for (i = 0; i < 10000; i++)
+        sum -= 1;
+    pthread_exit(0);
+}
+
+int main()
+{
+    pthread_t tid1, tid2;
+    pthread_create(&tid1, NULL, run1, NULL);
+    pthread_create(&tid2, NULL, run2, NULL);
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    printf("%d\n", sum);
+
+    return 0;
+}
